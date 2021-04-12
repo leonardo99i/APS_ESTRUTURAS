519c19ec9a0f580b2271d80e90f38fa84e0fba70
Leonardo Ribeiro Leonardi <49618344+Leo99z@users.noreply.github.com>
2021-04-12 17:21:33 -0300

    Gerando números aleatorios de 1 a 60, na fila.
    Co-Authored-By: Laura Moreira <75754131+lauraolimo@users.noreply.github.com>

--- Trabalho_APS_Estruturas/src/aps_estuturas/Main.java
+++ Trabalho_APS_Estruturas/src/aps_estuturas/Main.java
@@ -5,14 +5,23 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         TadFila fila = new TadFila(60);
-        int x = 60; 
-
+        int numeros;
+        int[] num = new int[20];
         Random gerador = new Random();
 
-        for(int i = 0; i < 20; i++){
-            fila.Enqueue(gerador.nextInt(x));
+        for(int i = 0; i<num.length; i++){
+            numeros = gerador.nextInt(60) + 1;
+            for(int j=0; j<num.length; j++){
+                if(numeros == num[j] && j != i){
+                    numeros = gerador.nextInt(60) + 1;
+                }else{
+                    num[i] = numeros;
+                }
+            }
         }
-
+        for(int i = 0; i < num.length; i++){
+            fila.Enqueue(num[i]);
+        }
         fila.imprimeFila();
     }
     
