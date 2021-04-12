c2291738b93d5ed441fb73a00de58e225d4976fd
Leonardo Ribeiro Leonardi <49618344+Leo99z@users.noreply.github.com>
2021-04-12 17:05:23 -0300

    bug fix

--- Trabalho_APS_Estruturas/src/aps_estuturas/Main.java
+++ Trabalho_APS_Estruturas/src/aps_estuturas/Main.java
@@ -5,12 +5,17 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         TadFila fila = new TadFila(60);
-        int x = 60; 
+         
 
         Random gerador = new Random();
+        int numero = gerador.nextInt(60);
 
-        for(int i = 0; i < 20; i++){
-            fila.Enqueue(gerador.nextInt(x));
+        for(int i = 0; i < 21; i++){
+            if(gerador.nextInt() == 0){
+                fila.Enqueue(gerador.nextInt(numero));
+            }else{
+                fila.Enqueue(gerador.nextInt(numero));
+            }
         }
 
         fila.imprimeFila();
